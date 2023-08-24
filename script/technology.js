5907e617f21957c73f54b07d1e60ad7427aed784
Sebastian <mrseb10@gmail.com>
2023-08-24 19:40:09 +0200

    Fixing...

--- script/technology.js
+++ script/technology.js
@@ -4,7 +4,7 @@ const technologyNamePlaceholder = document.querySelector('.technology-name');
 const technologyDescriptionPlaceholder = document.querySelector('.technology-description');
 // JSON technology data 
 const getTechnologyData = (index) => {
-  fetch('data.json')
+  fetch('/data.json')
   .then((res) => res.json()
   .then((data) => {
     technologyImagePlaceholder.innerHTML = `
