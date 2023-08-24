f2f3dbbaa55c7c16206b767d048edb1a3dc03c57
Sebastian <mrseb10@gmail.com>
2023-08-24 14:43:29 +0200

    JSON data ready to work with

--- script/app.js
+++ script/app.js
@@ -1,7 +1,25 @@
+// Elements
 const menuButton = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.nav-bar');
+const destinationButtons = document.querySelectorAll('.destination-button');
+  // JSON data placeholders
+  const planetImagePlaceholder = document.querySelector('.planet-image');
+  const planetNamePlaceholder = document.querySelector('.planet-name');
+  const planetDescriptionPlaceholder = document.querySelector('.planet-describtion');
+  const distancePlaceholder = document.querySelector('.distance');
+  const timePlaceholder = document.querySelector('.travel-time');
+  // JSON data 
+  const getData = () => {
+    fetch('/data.json')
+    .then((res) => res.json()
+    .then((data) => {
+      console.log(data.destinations);
+    }));
+  }
 
+getData();
 
+// mobile Menu folding
 menuButton.addEventListener('click', () => {
   if (menuButton.classList.contains('active')) {
     menu.classList.remove('shown');
@@ -10,4 +28,18 @@ menuButton.addEventListener('click', () => {
     menu.classList.add('shown');
     menuButton.classList.add('active');
   }
-})
+});
+// Destination picker
+destinationButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    toggleOff();
+    button.classList.add('is-toggled')
+  });
+});
+
+function toggleOff() {
+  destinationButtons.forEach((button) => {
+    button.classList.remove('is-toggled');
+  });
+};
+
