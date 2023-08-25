0d2f2a8cb29a8ac8fd1dcdd27e3a4ca5ef020e80
Sebastian <mrseb10@gmail.com>
2023-08-25 02:22:27 +0200

    Desktop | Done

--- script/technology.js
+++ script/technology.js
@@ -1,5 +1,6 @@
 // JSON technology data placeholders
-const technologyImagePlaceholder = document.querySelector('.technology-image');
+const technologyLandscapePlaceholder = document.querySelector('.technology-landscape');
+const technologyPortraitPlaceholder = document.querySelector('.technology-portrait');
 const technologyNamePlaceholder = document.querySelector('.technology-name');
 const technologyDescriptionPlaceholder = document.querySelector('.technology-description');
 // JSON technology data 
@@ -7,9 +8,12 @@ const getTechnologyData = (index) => {
   fetch('/data.json')
   .then((res) => res.json()
   .then((data) => {
-    technologyImagePlaceholder.innerHTML = `
+    technologyLandscapePlaceholder.innerHTML = `
     <img src=${data.technology[index].images.landscape} alt="">
     `;
+    technologyPortraitPlaceholder.innerHTML = `
+    <img src=${data.technology[index].images.portrait} alt="">
+    `
     technologyNamePlaceholder.innerHTML = data.technology[index].name;
     technologyDescriptionPlaceholder.innerHTML = data.technology[index].description;
   }));
